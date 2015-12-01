278250de6dfcf36df2f291781dcd95d26c6bf57c
Petr Faltus <Petr Faltus>
2015-12-01 10:33:17 +0100

    První verze

---
+++ PokusGH1.cs
@@ -0,0 +1,21 @@
+using System;
+
+namespace PokusGH1
+{
+	class Program
+	{
+		public static void Main(String[] args)
+		{
+			Console.WriteLine("Pokus GH 1");
+			
+			int len=args.Length;
+			
+			Console.WriteLine("- pocet argumentu: {0}", len);
+			if (len>0)
+			  for (int ii=0; ii<len; ii++)
+			    {
+				    Console.WriteLine(" -- {0}.parametr = {1}", ii+1, args[ii]);
+			    }
+		}
+	}
+}
