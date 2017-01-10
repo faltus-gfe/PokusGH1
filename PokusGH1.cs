0097dc7cf072ae8a35ef4e6b9353a0ea4f43b2eb
Petr Faltus <faltus@leitstelle112.de>
2017-01-10 17:14:36 +0100

    Nahrazení tabulátorů

--- PokusGH1.cs
+++ PokusGH1.cs
@@ -2,20 +2,18 @@ using System;
 
 namespace PokusGH1
 {
-	class Program
-	{
-		public static void Main(String[] args)
-		{
-			Console.WriteLine("Pokus GH 1");
-			
-			int len=args.Length;
-			
-			Console.WriteLine("- pocet argumentu: {0}", len);
-			if (len>0)
-			  for (int ii=0; ii<len; ii++)
-			    {
-				    Console.WriteLine(" -- {0}.parametr = {1}", ii+1, args[ii]);
-			    }
-		}
-	}
+    class Program
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine("Pokus GH 1");
+
+            int len = args.Length;
+
+            Console.WriteLine("- pocet argumentu: {0}", len);
+            if (len > 0)
+                for (int ii = 0; ii < len; ii++)
+                    Console.WriteLine(" -- {0}.parametr = {1}", ii + 1, args[ii]);
+        }
+    }
 }
